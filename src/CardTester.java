b9b021db9718b24e89ef375e436ec9d2e5341daf
Stephanie Kraft <stephaniekraft@Stephanies-Mac-mini.local>
2020-03-23 11:47:09 -0600

    implement card methods

--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,12 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
+		Card aceSpades = new Card("ace", "spades", 11);
+		Card twoDiamonds = new Card("two", "diamonds", 2);
+		Card jackClubs = new Card("jack", "clubs", 10);
 		
+		System.out.println(aceSpades);
+		System.out.println(twoDiamonds);
+		System.out.println(jackClubs);
 	}
 }
