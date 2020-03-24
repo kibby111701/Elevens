daf9b38073a6cdb496c4cc530f81734607400540
Stephanie Kraft <stephaniekraft@Stephanies-Mac-mini.local>
2020-03-24 13:41:29 -0600

    create perfect and efficient shuffle methods

--- src/Shuffler.java
+++ src/Shuffler.java
@@ -1,13 +1,15 @@
 /**
  * This class provides a convenient way to test shuffling methods.
  */
+import java.util.Random;
+
 public class Shuffler {
 
 	/**
 	 * The number of consecutive shuffle steps to be performed in each call
 	 * to each sorting procedure.
 	 */
-	private static final int SHUFFLE_COUNT = 1;
+	private static final int SHUFFLE_COUNT = 2;
 
 
 	/**
@@ -17,7 +19,7 @@ public class Shuffler {
 	public static void main(String[] args) {
 		System.out.println("Results of " + SHUFFLE_COUNT +
 								 " consecutive perfect shuffles:");
-		int[] values1 = {0, 1, 2, 3};
+		int[] values1 = {0, 1, 2, 3, 4};
 		for (int j = 1; j <= SHUFFLE_COUNT; j++) {
 			perfectShuffle(values1);
 			System.out.print("  " + j + ":");
@@ -30,7 +32,7 @@ public class Shuffler {
 
 		System.out.println("Results of " + SHUFFLE_COUNT +
 								 " consecutive efficient selection shuffles:");
-		int[] values2 = {0, 1, 2, 3};
+		int[] values2 = {0, 1, 2, 3, 4};
 		for (int j = 1; j <= SHUFFLE_COUNT; j++) {
 			selectionShuffle(values2);
 			System.out.print("  " + j + ":");
@@ -50,7 +52,21 @@ public class Shuffler {
 	 * @param values is an array of integers simulating cards to be shuffled.
 	 */
 	public static void perfectShuffle(int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		int[] shuffled = new int[values.length];
+		int k = 0;
+		for (int j = 0; j < (values.length + 1)/2; j++){
+			shuffled[k] = values[j];
+			k = k + 2;
+		}
+		k = 1;
+		for (int j = (values.length + 1)/2; j < values.length; j++){
+			shuffled[k] = values[j];
+			k = k + 2;
+		}
+
+		for (int j = 0; j < values.length; j++){
+			values[j] = shuffled[j];
+		}
 	}
 
 	/**
@@ -65,6 +81,15 @@ public class Shuffler {
 	 * @param values is an array of integers simulating cards to be shuffled.
 	 */
 	public static void selectionShuffle(int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		int temp = 0;
+		int r = 0;
+		Random rand = new Random();
+		for (int j = values.length-1; j > 0; j --){
+			r = rand.nextInt(j+1);
+			temp = values[j];
+			values[j] = values[r];
+			values[r] = temp;
+		}
+
 	}
 }
